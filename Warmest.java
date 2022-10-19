9dcaf9b33132588ad9ac2bd86a190208efde98b0
Caspiofri <106171029+Caspiofri@users.noreply.github.com>
2022-10-19 20:32:22 +0300

    Update Warmest.java

--- Warmest.java
+++ Warmest.java
@@ -8,61 +8,75 @@ class Node<K,V>
 	    Node<K,V> nextN;  
 	    Node<K,V> prevN;  
 	 
+	    public Node() {
+	    	this.key = null;
+	    	this.value = null; 
+	    	this.nextN = null; 
+	    	this.prevN = null; 
+	    }    
 	    public Node(K key,V value) {
 	    	this.key = key;
-	    	this.value = value; 	
-	    }    
-	    
+	    	this.value = value; 
+	    	this.nextN = null; 
+	    	this.prevN = null; 
+	    }       
 	}
       
 public class Warmest<K,V> {
-		
-	HashMap<K, Node<K,V>> map = new HashMap<K,Node<K,V>>();
-	private Node<K,V> head = new Node(null, null); 
-	private Node<K,V> tail  = new Node(null, null); 
 
-   public void insertNode(Node node) {
-			if (head.nextN == null ) {
-				head.nextN = node;
-				tail.prevN = node;
-				node.nextN =tail;
+	HashMap<K, Node<K,V>> map = new HashMap<K,Node<K,V>>();
+	private Node<K,V> head = new Node(); 
+	private Node<K,V> tail  = new Node();
+	
+	public Warmest() {
+		head.nextN = tail;
+		tail.prevN = head;
+	};
+	
+	public void insertNode(Node node) {
+			if (head.nextN == tail){
+				node.nextN = tail;
 				node.prevN = head;
+				tail.prevN = node;
+				head.nextN = node;
 			}
 			else { 
-				head.nextN.prevN = node;
-				head.nextN.prevN.nextN = head.nextN;
+				Node previous = head.nextN;
+				node.nextN = previous;
 				head.nextN = node;
-				head.nextN.prevN = head;
+				previous.prevN = node;
+				node.prevN = head;	
 			}	
 		}
-		
+
 		public void removeNode(Node node) {
-			if (head.nextN == null ) 
+			if (head.nextN == tail ) 
 				return;
-			if (node.nextN == null)
-				head.nextN =tail.prevN;
 			else {
-				node.nextN.prevN = node.prevN;
-				node.prevN.nextN = node.nextN;			
+				Node next = node.nextN;
+				next.prevN = node.prevN;
+				node = node.prevN;
+				node.nextN = next;
 			}
 		}	
 		    
 	public void put (K newKey, V newValue) {
-		Node node = new Node<K,V>(newKey,newValue);
 		if (map.containsKey(newKey)){
-			node = map.get(newKey);
+			Node node = map.get(newKey);
 			removeNode(node);
 			node.value = newValue;
 			insertNode(node);
 		}
-		else
+		else {
+			Node node = new Node<K,V>(newKey,newValue);
 			insertNode(node);
-		map.put(newKey, node);
+			map.put(newKey, node);
+		}
 	}
 		
 	public V get(K key) {
 		Node node = map.get(key);
-		if (node== null)
+		if (node == null)
 			return null;
 		removeNode(node);
 		insertNode(node);
@@ -82,18 +96,5 @@ public class Warmest<K,V> {
 		if (head.nextN == null)
 			return null;
 		return head.nextN.value;
-	}
-	
-	/*public void printList() {
-		if (head.nextN == null)
-			System.out.println("empty");
-		Node element = head.nextN;
-		while (element!=null) {
-			System.out.println(element.key);
-			element = element.nextN;
-		}
-		System.out.println("-----------------------");
-	}*/
+	}	
 }
-
-
