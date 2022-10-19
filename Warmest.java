16faf6b79ee136b0000e07925ba8142979732311
Caspiofri <106171029+Caspiofri@users.noreply.github.com>
2022-10-19 10:16:27 +0300

    Update Warmest.java

--- Warmest.java
+++ Warmest.java
@@ -1,27 +1,67 @@
 package question2;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-
 public class Warmest<K,V> {
-	
+		
 	private Entry<K, V>[] table; //setting an array of Entry
-	private int capacity = 16; //capacity of the modified hashmap.
-	HashMap<K, Integer> hotTable = new HashMap<K, Integer>();
-	LinkedList<Entry> hotElement = new LinkedList();
+	private int capacity = 16; //capacity of the modify	LinkedList<Entry> hotElement = new LinkedList();
 	
-	static class Entry<K, V>{ //Entry class -setting the key-value pairs
+	public Node head = null ; 
+	public Node tail = null ; 
+	
+	class Node{    
+	    K data;    
+        Node nextN;  
+        Node prevN;  
+     
+		// אני לא משתמשת בכל הסטרים\גטרים האלה.. שווה להשאיר ולנסות לעשות בהם שימוש בקוד או למחוק?
+	     public Node(K data) {    
+	         this.data = data;    
+	     } 
+	     
+	     public K getDataN()
+	     {
+	    	 return data;
+	     }
+	     public void setDataN(K newKey)
+	     {
+	    	this.data =  newKey;
+	     }
+	  
+	     public Node getNextN()
+	     {
+	    	return this.nextN;
+	     }
+	     
+	     public void setNextN(Node nextN)
+	     {
+	    	this.nextN =  nextN;
+	     }
+	     
+	     public Node getPrevN()
+	     {
+	    	return this.prevN;
+	     }
+	     
+	     public void setPrevN(Node PreviousN)
+	     {
+	    	this.prevN = PreviousN;
+	     }
+        
+	}     
+	
+	class Entry<K, V>{ //Entry class -setting the key-value pairs
 		
 		K key;
 		V value;
 		Entry<K, V> next;
+		Node hotElement;
 		
-		public Entry(K key , V value,Entry<K, V> next) {
+		public Entry(K key ,V value,Entry<K, V> next) {
 			this.key = key;
 			this.value = value;
 			this.next = next;
-			
 			}
+		
 		public K getKey() {
 			return key;
 		}
@@ -35,32 +75,57 @@ public class Warmest<K,V> {
 		public void setValue(V value) {
 			this.value = value;
 		}
+		
 		public Entry getNext() {
 			return next;
 		}
 		public void setNext(Entry<K, V> next) {
 			this.next = next;
 		}	
-	}
 	
+		public void insertNode(Entry<K, V> entry) {
+			if (head == null)
+				head = entry.hotElement;
+			else {
+			head.prevN = entry.hotElement;
+			entry.hotElement.nextN = head;
+			head = entry.hotElement;
+			entry.hotElement.prevN = null;
+			}	
+		}
+		
+		public void removeNode(Entry<K, V> entry) {
+			if (head != entry.hotElement) {
+				entry.hotElement.nextN.prevN = entry.hotElement.prevN;
+				entry.hotElement.prevN.nextN =  entry.hotElement.nextN;
+			}
+			else if (entry.hotElement.nextN != null) {
+				entry.hotElement.nextN.prevN = null;
+				head = entry.hotElement.nextN;
+			}
+			else
+				head =null;			
+		}	
+		
+	}
+
 	public Warmest() {
 		table = new Entry[capacity];
+		//Represent the head and tail of the singly linked list    
 	}
 
 	public void put (K newKey, V newValue) {
+		
 		if (newKey == null) {
 			return; //avoiding storing null.
 		}
-		
 		//calculating index of key.
 		int index = newKey.hashCode() % capacity; 
 		 // creating new entry with the new key and value.
 		Entry<K, V> newEntry = new Entry(newKey,newValue,null);
 		if (table[index] == null) {
 			table[index] =  newEntry; //if the index in table doesn't contain any entry - store entry in index.
-			hotElement.add(newEntry);
-			int hotKey = hotElement.indexOf(newEntry);
-			hotTable.put(newKey, hotKey);
+			
 		}
 		else { //if the key already in the list - change its value.
 			Entry<K, V> previous = null;
@@ -77,7 +142,17 @@ public class Warmest<K,V> {
 			//creating new slot for the next pair
 			if (previous != null)
 				previous.setNext(newEntry);			
-		}	
+		}
+		newEntry.hotElement = new Node(newKey);
+		if (head == null) {
+			head = newEntry.hotElement;
+            tail = newEntry.hotElement;
+            //head.prevN = null;
+            //tail.nextN = null;
+		}
+		else {
+			newEntry.insertNode(newEntry);
+		}		
 	}
 
 	public V get(K key) {
@@ -86,16 +161,13 @@ public class Warmest<K,V> {
 		//calculating index of key.
 		int index = key.hashCode() % capacity;
 		Entry<K, V> entry = table[index];
-		
 		while (entry != null) {
 			if (entry.getKey().equals(key)) {
 				value = entry.getValue();
-				int hotKey = hotTable.get(key);
-				
-				hotElement.set(hotKey, null);
-				hotElement.add(entry);
-				hotKey = hotElement.indexOf(entry);
-				hotTable.put(key, hotKey);
+				//removing node from list 
+				entry.removeNode(entry);
+				//inserting node at the head of the list 
+				entry.insertNode(entry);
 				break;
 			}
 			entry = entry.getNext();
@@ -111,7 +183,9 @@ public class Warmest<K,V> {
 		if(entry == null)
 			return null;
 		V value = entry.getValue();
-		int hotKey = hotTable.get(key);
+		//deleting the hotElement from the list
+		entry.removeNode(entry);
+		
 		while(entry != null) {
 			if(entry.getKey().equals(key)) {
 				if (previous == null) {
@@ -126,31 +200,32 @@ public class Warmest<K,V> {
 			previous = entry;
 			entry = entry.getNext();
 		}
-		if (hotElement.peekLast().key == key )
-		{
-			hotElement.removeLast();
-			hotTable.remove(key);
-		}
-		else{
-			hotElement.set(hotKey, null);
-			hotTable.replace(key, null);
-		}
-		while (hotElement.peekLast() == null && hotElement.isEmpty() == false) {
-			hotElement.removeLast();
-		}
 		return value;
 	}
 		
 		
 	public V getWarmest() {
-		if (hotElement.isEmpty())
-		{
+		if (head == null)
 			return null;
-		}
-		Entry<K,V> hotEntry = hotElement.getLast();
-		V value = hotEntry.value;
-		
+		K key = head.data;
+		int index = key.hashCode() % capacity;
+		Entry<K, V> entry = table[index];
+		if(entry == null)
+			return null;
+		V value = entry.getValue();	
 		return value;	
 	}
-
+	
+	public void printList() {
+		if (head == null)
+			System.out.println("empty");
+		Node element = head;
+		while (element!=null) {
+			System.out.println(element.data);
+			element = element.nextN;
+		}
+		
+	}
 }
+
+
